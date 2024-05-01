e13dbcd0306a51d4a1b8296f44c42253926cf545
zcube <zcubekr@gmail.com>
2024-05-01 17:18:51 +0900

    feat: init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# UnityPlayerLinuxFontPatch
+
+* for Steam Linux Runtime Only
+* this patch is for UnityPlayer.so
+* change font path to /run/host/fonts
+* target is "~/.local/share/Steam/steamapps/common/**/UnityPlayer.so"
+* Python 3 required
+
+# Usage
+
+```bash
+./patch.py
+```
+
+# License
+
+MIT License
---
+++ patch.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+def patch_unityplayer_so(unityplayer_so_path):
+    print(f"Patching {unityplayer_so_path}")
+    with open(unityplayer_so_path, "rb") as f:
+        data = f.read()
+        data = data.replace(b"/usr/share/fonts", b"/run/host/fonts/")
+    with open(unityplayer_so_path, "wb") as f:
+        f.write(data)
+
+def patch_unityplayer_so_recursive(path):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file == "UnityPlayer.so":
+                patch_unityplayer_so(os.path.join(root, file))
+def main():
+    steamCommonPath = "~/.local/share/Steam/steamapps/common/"
+    steamCommonPath = os.path.expanduser(steamCommonPath)
+    patch_unityplayer_so_recursive(steamCommonPath)
+
+if __name__ == "__main__":
+    main()
